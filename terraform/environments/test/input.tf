b03a32f3ead0f9d844e9194e4714f6c0798032b7
TuanNA163 <tuannguyen.dev193@gmail.com>
2024-11-03 23:34:24 +0700

    update config

--- terraform/environments/test/input.tf
+++ terraform/environments/test/input.tf
@@ -1,11 +1,16 @@
 # Azure GUIDS
 variable "subscription_id" {
+    default = "46f0b4f7-e5d8-488c-b438-00f74a74c84a"
 }
 variable "client_id" {
+    default = "b869c436-a2f3-4198-846a-65564baa48f8"
 }
 variable "client_secret" {
+    description = "The client secret"
+    default = "YId8Q~-jemt3gKMb-YtN_yjox0vr_sk634OCocnW"
 }
 variable "tenant_id" {
+    default = "f958e84a-92b8-439f-a62d-4f45996b6d07"
 }
 
 # Resource Group/Location
